5a6ed679f12d7e1d2217a868abb197001648848a
Chinmay <craval@ripple.com>
2019-05-07 16:02:03 -0700

    adding uip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactJson from 'react-json-view';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: null,
+      response: null
+    }
+    this.loadData = this.loadData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
+  async loadData() {
+    const response = await axios.get('/ilp?hash=' + this.state.input);
+    this.setState({ response })
+  }
+
   render() {
+    const { response } = this.state;
+    const json = response && <ReactJson src={response} />
     return (
       <div className="App">
-        <ReactJson src={jsonPacket} />
+        <label>
+          hash:
+          <input type="text" value={this.state.input} onChange={this.handleChange} />
+        </label>
+        <button onClick={this.loadData}>load data</button>
+        {json}
       </div>
     );
   }
